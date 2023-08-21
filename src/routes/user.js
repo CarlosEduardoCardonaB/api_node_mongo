40ba2cad8d1f83054503270a3a7de57a389f42ba
Carlos Eduardo <caeca425@gmail.com>
2023-08-20 21:22:44 -0500

    completa api CRUD con mongo

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,6 +15,45 @@ router.post('/user', (req, res)=>{
     .then((data)=> res.json(data))
     .catch((error)=> res.json({message: error}))
     //res.send('post - create user')
-})
+});
 
+//GET all users
+router.get('/users', (req, res)=>{
+    userSchema
+    .find()
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    //res.send('post - create user')
+});
+
+//GET one user
+router.get('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    userSchema
+    .findById(id)
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    //res.send('post - create user')
+});
+
+//Update one user
+router.put('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    const { name, age, email } = req.body;
+    userSchema
+    .updateOne({ _id: id }, { $set: {name, age, email}})
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    //res.send('post - create user')
+});
+
+//Delete one user
+router.delete('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    userSchema
+    .deleteOne({ _id: id})
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    //res.send('post - create user')
+});
 module.exports = router;
